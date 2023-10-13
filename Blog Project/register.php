b3243d5894676da0cf4c68f5e301c4e03f2d3b2c
Krzysztof Blazewicz <121895984+Turrz@users.noreply.github.com>
2023-10-13 15:44:36 +0100

    Update register.php

--- Blog Project/register.php
+++ Blog Project/register.php
@@ -1,5 +1,8 @@
 <?php
     require "config/database.php";
+    if (count($_COOKIE) > 0) {
+        header("Location:index.php");
+    }
     if(isset($_POST['btnRegister']))
     {
         $first_name = $_POST['txtFirstName']; 
@@ -13,24 +16,23 @@
     }
 ?>
 
-
 <?php include "includes/header.php"; ?>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
     <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">1</span>
+        <span class="input-group-text" id="basic-addon1">*</span>
         <input type="text" class="form-control" placeholder="First Name" aria-label="firstName" aria-describedby="basic-addon1" name="txtFirstName" required>
     </div>
     <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">2</span>
+        <span class="input-group-text" id="basic-addon1">*</span>
         <input type="text" class="form-control" placeholder="Last Name" aria-label="lastName" aria-describedby="basic-addon1" name="txtLastName" required>
     </div>
     <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">3</span>
+        <span class="input-group-text" id="basic-addon1">*</span>
         <input type="email" class="form-control" placeholder="Email" aria-label="email" aria-describedby="basic-addon1" name="txtEmail" required>
     </div>
     <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">4</span>
+        <span class="input-group-text" id="basic-addon1">*</span>
         <input type="password" class="form-control" placeholder="Password" aria-label="password" aria-describedby="basic-addon1" name="txtPassword" required>
     </div>
     <div>
