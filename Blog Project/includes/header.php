69243471cb184602c622ed0a048659d0cd97c992
Krzysztof Blazewicz <121895984+Turrz@users.noreply.github.com>
2023-10-27 15:07:14 +0100

    Update header.php

--- Blog Project/includes/header.php
+++ Blog Project/includes/header.php
@@ -1,3 +1,6 @@
+
+<!-- Header -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,8 +43,7 @@
           <li>
       </ul>
       <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        
       </form>
           </li>
         </li>
