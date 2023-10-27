a0dd78da3d220e68065954d832953b6ad2844773
Krzysztof Blazewicz <121895984+Turrz@users.noreply.github.com>
2023-10-27 15:07:58 +0100

    Update index.php

--- Blog Project/index.php
+++ Blog Project/index.php
@@ -1,4 +1,32 @@
+<!-- Home Page -->
+
 <?php include "includes/header.php"; ?>
 
-<?php include "includes/footer.php"; ?>
+<?php 
 
+    require 'config/database.php';
+
+    $page = 1;
+    $start = 0;
+
+    if(isset($_GET['page']))
+    {
+        $page = $_GET['page'];
+        $start = ($page - 1) * 10;
+    }
+
+    $end = $page * 10;
+
+    $sql = "SELECT * FROM blogs ORDER BY release_date DESC LIMIT 10 $start";
+
+    $result = mysqli_query($conn, $sql);
+
+    while($row = mysqli_fetch_assoc($result))
+    {
+        var_dump($row);
+        echo "<br><br>";
+    }
+
+?>
+
+<?php include "includes/footer.php"; ?>
